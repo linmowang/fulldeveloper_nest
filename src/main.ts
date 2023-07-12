eb3c71bf3e3385c7b1f498b9436fce4771429d52
linjie <linjie@hifunfun.com>
2023-07-12 23:27:41 +0800

    add cors
    add cors middleware make test simple

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
 
   // swagger start
   const options = new DocumentBuilder()
