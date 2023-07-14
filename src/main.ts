e7de3831220395d981ad458d8080dbfa16350e3c
linjie <linjie@hifunfun.com>
2023-07-14 21:17:10 +0800

    add session to login
    use session
    use svg-captcha generator code
    done login

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cors from 'cors';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
+  app.use(
+    session({
+      resave: true,
+      saveUninitialized: true,
+      secret: 'prev',
+      rolling: true,
+      name: 'login.sid',
+      cookie: {
+        maxAge: null,
+      },
+    }),
+  );
 
   // swagger start
   const options = new DocumentBuilder()
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,14 +7,50 @@ import {
   Param,
   Delete,
   Query,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as svgCaptcha from 'svg-captcha';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('code')
+  createCode(@Req() req, @Res() res) {
+    const captcha = svgCaptcha.create({
+      size: 4,
+      fontSize: 50,
+      width: 100,
+      height: 34,
+      background: '#cc9966',
+    });
+
+    req.session.code = captcha.text;
+    res.type('image/svg+xml');
+    res.send(captcha.data);
+  }
+
+  @Post('create')
+  createUser(@Req() req, @Body() body) {
+    console.log(body, req.session);
+    if (
+      req.session.code.toLocaleLowerCase() === body.code.toLocaleLowerCase()
+    ) {
+      return {
+        code: 200,
+        message: '验证码正确',
+      };
+    } else {
+      return {
+        code: 200,
+        message: '验证码错误',
+      };
+    }
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
